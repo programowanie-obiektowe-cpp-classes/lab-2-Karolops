6e7c244c90aed52f65cc56695a515bb8ce939cc5
Karol_Niedbało <133537761+Karolops@users.noreply.github.com>
2023-11-16 13:19:50 +0100

    Update ResourceManager.hpp

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -4,5 +4,31 @@
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+        Resource* resource;
+
+    public:
+    // konstruktor
+    ResourceManager(){   
+        resource = new Resource{};
+    }
+    // destruktor
+    ~ResourceManager(){    
+        delete resource;
+    }
+
+    ResourceManager(const ResourceManager& resource1){
+        resource = new Resource{};
+        *resource = *resource1.resource;
+    }
+
+    ResourceManager& operator=(const ResourceManager& resource2){
+        delete this->resource;
+        resource = new Resource{};
+        *resource = *resource2.resource;
+        return *this;
+    }
+    double get() 
+    { 
+        return resource->get(); 
+    }
 };
